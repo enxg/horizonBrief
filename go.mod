66e5c994be55794407cc33db421972216a6666dc
Enes Genç <hello@enesgenc.dev>
2025-10-04 11:33:42 +0300

    feat: implement main application logic

--- go.mod
+++ go.mod
@@ -1,3 +1,60 @@
 module horizonBrief
 
-go 1.25
+go 1.25.0
+
+require (
+	github.com/bytedance/sonic v1.14.1
+	github.com/ebitengine/oto/v3 v3.3.3
+	github.com/gofiber/fiber/v3 v3.0.0-rc.2
+	github.com/joho/godotenv v1.5.1
+	google.golang.org/api v0.251.0
+	google.golang.org/genai v1.28.0
+)
+
+require (
+	cloud.google.com/go v0.116.0 // indirect
+	cloud.google.com/go/auth v0.16.5 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.8 // indirect
+	cloud.google.com/go/compute/metadata v0.9.0 // indirect
+	github.com/andybalholm/brotli v1.2.0 // indirect
+	github.com/bytedance/gopkg v0.1.3 // indirect
+	github.com/bytedance/sonic/loader v0.3.0 // indirect
+	github.com/cloudwego/base64x v0.1.6 // indirect
+	github.com/ebitengine/purego v0.8.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/fxamacker/cbor/v2 v2.9.0 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gofiber/schema v1.6.0 // indirect
+	github.com/gofiber/utils/v2 v2.0.0-rc.1 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
+	github.com/google/s2a-go v0.1.9 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.6 // indirect
+	github.com/googleapis/gax-go/v2 v2.15.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/philhofer/fwd v1.2.0 // indirect
+	github.com/tinylib/msgp v1.4.0 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.65.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.61.0 // indirect
+	go.opentelemetry.io/otel v1.37.0 // indirect
+	go.opentelemetry.io/otel/metric v1.37.0 // indirect
+	go.opentelemetry.io/otel/trace v1.37.0 // indirect
+	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
+	golang.org/x/crypto v0.42.0 // indirect
+	golang.org/x/net v0.44.0 // indirect
+	golang.org/x/oauth2 v0.31.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250929231259-57b25ae835d4 // indirect
+	google.golang.org/grpc v1.75.1 // indirect
+	google.golang.org/protobuf v1.36.9 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,271 @@
 package main
 
-func main() {
+import "C"
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
 
+	"github.com/bytedance/sonic"
+	"github.com/ebitengine/oto/v3"
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/client"
+	_ "github.com/joho/godotenv/autoload"
+	"google.golang.org/api/calendar/v3"
+	"google.golang.org/api/option"
+	"google.golang.org/genai"
+)
+
+type ConfigUser struct {
+	Name string `json:"name"`
+}
+
+type ConfigLocation struct {
+	Name                string  `json:"name"`
+	FriendlyDescription string  `json:"friendly_description"`
+	Latitude            float64 `json:"latitude"`
+	Longitude           float64 `json:"longitude"`
+	Notes               string  `json:"notes"`
+}
+
+type ConfigCalendar struct {
+	ID    string `json:"id"`
+	Notes string `json:"notes"`
+}
+
+type ConfigGemini struct {
+	Text  TextModel  `json:"text"`
+	Voice VoiceModel `json:"voice"`
+}
+
+type TextModel struct {
+	Model  string `json:"model"`
+	Prompt string `json:"prompt"`
+}
+
+type VoiceModel struct {
+	Model  string `json:"model"`
+	Prompt string `json:"prompt"`
+	Voice  string `json:"voice"`
+}
+
+type Config struct {
+	User      ConfigUser       `json:"user"`
+	Locations []ConfigLocation `json:"locations"`
+	Calendars []ConfigCalendar `json:"calendars"`
+	Gemini    ConfigGemini     `json:"gemini"`
+}
+
+type User struct {
+	Name     string `json:"name"`
+	DateTime string `json:"dateTime"`
+	Day      string `json:"day"`
+}
+
+type Location struct {
+	Name                string         `json:"name"`
+	FriendlyDescription string         `json:"friendly_description"`
+	Notes               string         `json:"notes"`
+	WeatherData         map[string]any `json:"weather_data"`
+}
+
+type Calendar struct {
+	Name   string         `json:"name"`
+	Notes  string         `json:"notes"`
+	Events map[string]any `json:"events"`
+}
+
+type AIData struct {
+	User      User       `json:"user"`
+	Locations []Location `json:"locations"`
+	Calendars []Calendar `json:"calendars"`
+}
+
+const weatherAPIUrl = "https://weather.googleapis.com/v1/forecast/hours:lookup?key=%s&location.latitude=%f&location.longitude=%f&hours=%d"
+
+func main() {
+	ctx := context.Background()
+
+	app := fiber.New()
+	cc := client.New()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	weatherAPIKey := os.Getenv("WEATHER_API_KEY")
+	if weatherAPIKey == "" {
+		panic("WEATHER_API_KEY environment variable is not set")
+	}
+
+	var config Config
+	data, err := os.ReadFile("config.json")
+	if err != nil {
+		panic(err)
+	}
+	err = sonic.Unmarshal(data, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	serviceCredentialsFile := "./service_account.json"
+	cal, err := calendar.NewService(ctx, option.WithCredentialsFile(serviceCredentialsFile))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Calendar client: %v", err)
+	}
+
+	gen, err := genai.NewClient(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	textConfig := &genai.GenerateContentConfig{
+		SystemInstruction: genai.NewContentFromText(config.Gemini.Text.Prompt, genai.RoleUser),
+	}
+
+	voiceConfig := &genai.GenerateContentConfig{
+		ResponseModalities: []string{"AUDIO"},
+		SpeechConfig: &genai.SpeechConfig{
+			VoiceConfig: &genai.VoiceConfig{
+				PrebuiltVoiceConfig: &genai.PrebuiltVoiceConfig{
+					VoiceName: config.Gemini.Voice.Voice,
+				},
+			},
+		},
+	}
+
+	app.Get("/day", func(c fiber.Ctx) error {
+		ti := time.Now().In(time.Local)
+		//ti := time.Unix(1759841121, 0).In(time.Local) // For testing purposes
+		tis := ti.Format(time.RFC3339)
+
+		aiData := AIData{
+			User: User{
+				Name:     config.User.Name,
+				DateTime: tis,
+				Day:      ti.Weekday().String(),
+			},
+		}
+
+		for _, location := range config.Locations {
+			res, err := cc.Get(fmt.Sprintf(weatherAPIUrl, weatherAPIKey, location.Latitude, location.Longitude, 24))
+			if err != nil {
+				return err
+			}
+
+			body := make(map[string]any)
+			if err := res.JSON(&body); err != nil {
+				return err
+			}
+
+			aiData.Locations = append(aiData.Locations, Location{
+				Name:                location.Name,
+				FriendlyDescription: location.FriendlyDescription,
+				Notes:               location.Notes,
+				WeatherData:         body,
+			})
+		}
+
+		startOfDay := time.Date(ti.Year(), ti.Month(), ti.Day(), 0, 0, 0, 0, ti.Location())
+		startOfTomorrow := startOfDay.Add(24 * time.Hour)
+		timeMin := startOfDay.Format(time.RFC3339)
+		timeMax := startOfTomorrow.Format(time.RFC3339)
+
+		for _, c := range config.Calendars {
+			ci, err := cal.Calendars.Get(c.ID).Do()
+			if err != nil {
+				log.Printf("Unable to retrieve calendar: %v", err)
+				continue
+			}
+
+			events, err := cal.Events.
+				List(c.ID).
+				ShowDeleted(false).
+				SingleEvents(true).
+				TimeMin(timeMin).
+				TimeMax(timeMax).
+				OrderBy("startTime").
+				Do()
+			if err != nil {
+				log.Printf("Unable to retrieve next ten of the user's events: %v", err)
+				continue
+			}
+
+			aiData.Calendars = append(aiData.Calendars, Calendar{
+				Name:  ci.Summary,
+				Notes: c.Notes,
+				Events: map[string]any{
+					"items": events.Items,
+				},
+			})
+		}
+
+		aiDataText, err := sonic.Marshal(aiData)
+		if err != nil {
+			return err
+		}
+
+		text, err := gen.Models.GenerateContent(
+			ctx,
+			config.Gemini.Text.Model,
+			genai.Text(string(aiDataText)),
+			textConfig,
+		)
+		if err != nil {
+			return err
+		}
+
+		voice, err := gen.Models.GenerateContent(
+			ctx,
+			config.Gemini.Voice.Model,
+			genai.Text(config.Gemini.Voice.Prompt+text.Text()),
+			voiceConfig,
+		)
+		if err != nil {
+			return err
+		}
+
+		meta := voice.Candidates[0].Content.Parts[0].InlineData
+
+		op := &oto.NewContextOptions{}
+		/*_, err = fmt.Sscanf(meta.MIMEType, "audio/L16;codec=pcm;rate=%d", &op.SampleRate)
+		if err != nil {
+			return err
+		}*/
+		op.SampleRate = 24000
+		op.ChannelCount = 1
+		op.Format = oto.FormatSignedInt16LE
+
+		audioReader := bytes.NewReader(meta.Data)
+
+		go func() {
+			println("Preparing audio...")
+
+			otoCtx, readyChan, err := oto.NewContext(op)
+			if err != nil {
+				panic("oto.NewContext failed: " + err.Error())
+			}
+
+			<-readyChan
+
+			println("Playing audio...")
+
+			player := otoCtx.NewPlayer(audioReader)
+			player.Play()
+
+			for player.IsPlaying() {
+				time.Sleep(time.Millisecond)
+			}
+
+			defer player.Close()
+		}()
+
+		return c.JSON(text)
+	})
+
+	app.Listen(":" + port)
 }
